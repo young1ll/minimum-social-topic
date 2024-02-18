3223a08855e58fdec4fe0528a07d7032acdeb47a
young1ll <122770896+young1ll@users.noreply.github.com>
2024-02-19 07:49:47 +0900

    fix redis config

--- src/utils/redis/index.ts
+++ src/utils/redis/index.ts
@@ -3,9 +3,9 @@ import { redisConfig } from '@/config';
 
 export const redisConnection = new Redis({
     // host: 'topic-redis',
-    port: 6379,
+    // port: 6379,
     // host: 'topic-app-topic-redis-1',
-    host: 'noderedis',
+    // host: 'noderedis',
     // host: redisConfig.HOST || '0.0.0.0',
     // port: redisConfig.PORT,
     // username: redisConfig.USER,
