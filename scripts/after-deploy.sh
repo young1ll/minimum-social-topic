f1ebe439196662674ff7fa7e3da3e59ee5d68e90
young1ll <122770896+young1ll@users.noreply.github.com>
2024-02-19 09:34:16 +0900

    fix after-deploy script

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -3,4 +3,15 @@ REPOSITORY=/home/ubuntu/topic-app
 
 cd $REPOSITORY
 
+# 기존 컨테이너 확인 후 중지 및 삭제
+if docker ps -a | grep -q noderedis; then
+    docker stop noderedis
+    docker rm noderedis
+fi
+
+if docker ps -a | grep -q nodeapp; then
+    docker stop nodeapp
+    docker rm nodeapp
+fi
+
 sudo docker-compose up -d
